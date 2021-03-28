e33b21e5ec744ee3095ce50747faf038c824a42e
mohamed <elkhazen1@gmail.com>
2021-03-28 11:08:18 -0500

    part one is done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://engineering.wustl.edu/news/magazine/images/Launchcode-icon.jpg';
+  image3 = 'https://pbs.twimg.com/profile_images/1295809711393177606/zJcb95MI_400x400.jpg';
 
   constructor() { }
 
