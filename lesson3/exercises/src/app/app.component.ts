ba6e0845ee81e569d29b45ce6f94825cb816c64e
mohamed <mohammed.elkhazen@gmail.com>
2021-04-07 18:06:59 -0500

    update

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,52 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  up = 0;
+  down = 0;
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+ }
+
+ handleLand(){
+  let result = window.confirm('The shuttle is landing. Landing gear engaged.');
+  if (result) {
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'The shuttle has landed.';
+ }
+ }
+
+ handleAbrotMission(){
+  let result = window.confirm('Are you sure you want to Abrot this Mission?');
+  if (result) {
+    this.color = 'red';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'Mission aborted.';
+ }
+ }
+
+ handleUpButton(){
+   this.up += 10;
+   this.down += 10;
+ }
+ handleDownButton(){
+   if (this.up > 0 || this.down > 0){
+    this.up -= 10
+    this.down -= 10
+   }
+   console.log(this.up)
+
+ }
+
+
 }
+
